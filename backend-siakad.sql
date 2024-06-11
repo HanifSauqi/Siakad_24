196eb58d02b75aea79abad67369af0b173f14bfa
HanifSauqi <haniffsaukii@gmail.com>
2024-06-11 23:15:07 +0700

    Update value sql

--- backend-siakad.sql
+++ backend-siakad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 05:02 AM
+-- Generation Time: Jun 11, 2024 at 06:13 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -30,22 +30,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `data_dosen` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
   `foto` varchar(255) DEFAULT NULL,
   `NIDN` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `telepon` varchar(255) NOT NULL,
-  `kewarganegaraan` varchar(255) NOT NULL,
-  `id_pendidikan` bigint(20) UNSIGNED NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `tgl_lahir` date NOT NULL,
   `nip` varchar(255) NOT NULL,
-  `id_jabatan` bigint(20) UNSIGNED NOT NULL,
-  `keahlian` varchar(255) NOT NULL,
-  `status_keaktifan` varchar(255) NOT NULL,
-  `status_pekerjaan` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telepon` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `kewarganegaraan` varchar(255) DEFAULT NULL,
+  `id_pendidikan` bigint(20) UNSIGNED DEFAULT NULL,
+  `status` varchar(255) NOT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `id_jabatan` bigint(20) UNSIGNED DEFAULT NULL,
+  `keahlian` varchar(255) DEFAULT NULL,
+  `status_keaktifan` varchar(255) DEFAULT NULL,
+  `status_pekerjaan` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -54,8 +54,8 @@ CREATE TABLE `data_dosen` (
 -- Dumping data for table `data_dosen`
 --
 
-INSERT INTO `data_dosen` (`id`, `user_id`, `foto`, `NIDN`, `nama`, `jenis_kelamin`, `email`, `telepon`, `kewarganegaraan`, `id_pendidikan`, `status`, `alamat`, `tgl_lahir`, `nip`, `id_jabatan`, `keahlian`, `status_keaktifan`, `status_pekerjaan`, `created_at`, `updated_at`) VALUES
-(2, 1, NULL, '1234567890', 'John Doe', 'Laki-laki', 'johndoe@example.com', '081234567890', 'Indonesia', 1, 'Aktif', 'Jl. Contoh, No. 1', '1990-01-01', '1234567890', 1, 'Informatika', 'Aktif', 'Dosen', '2024-03-21 20:37:18', '2024-03-21 20:37:18');
+INSERT INTO `data_dosen` (`id`, `user_id`, `nama`, `foto`, `NIDN`, `nip`, `email`, `telepon`, `jenis_kelamin`, `alamat`, `kewarganegaraan`, `id_pendidikan`, `status`, `tgl_lahir`, `id_jabatan`, `keahlian`, `status_keaktifan`, `status_pekerjaan`, `created_at`, `updated_at`) VALUES
+(1, 1, 'farhan', 'jkgwHBsVJSCGMqRhmUXm.jpg', '12323', '09890', NULL, NULL, 'laki-laki', 'Semarang', NULL, NULL, 'aktif', NULL, NULL, NULL, NULL, NULL, '2024-05-16 18:25:51', '2024-05-16 18:25:51');
 
 -- --------------------------------------------------------
 
@@ -87,10 +87,10 @@ CREATE TABLE `data_mahasiswa` (
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `nama` varchar(255) NOT NULL,
   `jenis_kelamin` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telepon` varchar(255) DEFAULT NULL,
   `foto` varchar(255) NOT NULL,
-  `kewarganegaraan` varchar(255) NOT NULL,
+  `kewarganegaraan` varchar(255) DEFAULT NULL,
   `angkatan` year(4) NOT NULL,
   `semester` bigint(20) UNSIGNED NOT NULL,
   `prodi_id` bigint(20) UNSIGNED NOT NULL,
@@ -98,8 +98,8 @@ CREATE TABLE `data_mahasiswa` (
   `alamat` text NOT NULL,
   `tgl_lahir` date NOT NULL,
   `nim` varchar(255) NOT NULL,
-  `jenjang_id` bigint(20) UNSIGNED NOT NULL,
-  `kelas_id` bigint(20) UNSIGNED NOT NULL,
+  `jenjang_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `kelas_id` bigint(20) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -109,17 +109,8 @@ CREATE TABLE `data_mahasiswa` (
 --
 
 INSERT INTO `data_mahasiswa` (`id`, `user_id`, `nama`, `jenis_kelamin`, `email`, `telepon`, `foto`, `kewarganegaraan`, `angkatan`, `semester`, `prodi_id`, `status`, `alamat`, `tgl_lahir`, `nim`, `jenjang_id`, `kelas_id`, `created_at`, `updated_at`) VALUES
-(3, 1, 'Michael jackson', 'Laki-laki', 'michael@example.com', '08123456789', 'link_ke_foto', 'Indonesia', 2020, 1, 1, 'Aktif', 'Jl. Contoh No. 1', '2000-01-01', '12345678', 1, 1, '2024-03-14 18:13:37', '2024-03-14 18:16:28'),
-(5, 1, 'Jordyn Kautzer', 'Laki-laki', 'qkozey@example.net', '+1.763.418.4984', 'default.jpg', 'Indonesia', 2000, 1, 1, 'Aktif', '37802 Briana Crossing Apt. 987\nCroninville, IA 61072', '1996-02-19', '4851447472', 1, 1, NULL, NULL),
-(6, 1, 'Daija Koch', 'Laki-laki', 'emerald.wyman@example.net', '940.421.9214', 'default.jpg', 'Indonesia', 2015, 1, 1, 'Aktif', '888 Katrina Keys Apt. 338\nEast Darius, VA 11718', '1993-04-02', '4755823920', 1, 1, NULL, NULL),
-(7, 1, 'Josefina Durgan', 'Laki-laki', 'nbeatty@example.net', '+1-530-205-3912', 'default.jpg', 'Indonesia', 2015, 1, 1, 'Aktif', '40910 Gorczany Vista Apt. 380\nSouth Tate, MI 59499', '1986-03-06', '4373620348', 1, 1, NULL, NULL),
-(8, 1, 'Tia Metz', 'Laki-laki', 'lueilwitz.paul@example.com', '601.550.3352', 'default.jpg', 'Indonesia', 2010, 1, 1, 'Aktif', '59953 Hermina Estates Suite 164\nPort Eleanoramouth, WI 94861-3130', '1970-08-16', '7761717650', 1, 1, NULL, NULL),
-(9, 1, 'Miss Amelia Padberg II', 'Perempuan', 'kjacobson@example.net', '+1 (442) 828-8819', 'default.jpg', 'Indonesia', 2007, 1, 1, 'Aktif', '9131 Cummings Ford\nWest Ibrahim, IL 36967-4644', '1976-10-17', '8803244964', 1, 1, NULL, NULL),
-(10, 1, 'Ida Towne', 'Perempuan', 'xoconnell@example.net', '281-588-4079', 'default.jpg', 'Indonesia', 2019, 1, 1, 'Aktif', '66453 Baumbach Fords Apt. 367\nMinniestad, WV 12871-0602', '1997-05-17', '5691251013', 1, 1, NULL, NULL),
-(11, 1, 'Naomi West', 'Laki-laki', 'sawayn.johathan@example.net', '1-530-804-8109', 'default.jpg', 'Indonesia', 2001, 1, 1, 'Aktif', '919 Esther Causeway Suite 187\nDibbertfurt, CA 16061-8704', '1976-12-16', '0084429236', 1, 1, NULL, NULL),
-(12, 1, 'Prof. Eloy Bednar', 'Laki-laki', 'colin50@example.org', '+1.346.628.7773', 'default.jpg', 'Indonesia', 2017, 1, 1, 'Aktif', '240 Hirthe Mountains Apt. 533\nLake Lew, AZ 01609', '1982-07-05', '7683119781', 1, 1, NULL, NULL),
-(13, 1, 'Dr. Stan Stanton', 'Perempuan', 'bradtke.ralph@example.net', '321-280-3111', 'default.jpg', 'Indonesia', 2021, 1, 1, 'Aktif', '23782 Lindgren Curve Suite 690\nJeramieport, RI 51378-7491', '1978-12-30', '1130288930', 1, 1, NULL, NULL),
-(14, 1, 'Dewitt Lemke', 'Perempuan', 'richmond69@example.org', '+1 (603) 948-6757', 'default.jpg', 'Indonesia', 2003, 1, 1, 'Aktif', '6204 Terry Parkway Apt. 086\nReillychester, SD 62971-5467', '1998-10-22', '9153865852', 1, 1, NULL, NULL);
+(22, 1, 'farhan', 'laki-laki', NULL, NULL, 'MSrqghHpvVmsa0NgJiLB.png', NULL, 2022, 1, 1, 'aktif', 'sasadasad', '2024-05-02', 'NIM1', NULL, NULL, '2024-05-09 09:35:43', '2024-05-09 09:35:43'),
+(24, 1, 'Muhammad hanif sauqi', 'laki-laki', NULL, NULL, 'oG9jvi985u0KAc5eeg4M.jpg', NULL, 2022, 4, 1, 'aktif', 'Semarang', '2003-02-17', '22/495097/SV/20949', NULL, NULL, '2024-05-16 23:19:29', '2024-05-16 23:19:29');
 
 -- --------------------------------------------------------
 
@@ -144,11 +135,7 @@ CREATE TABLE `data_matkul` (
 --
 
 INSERT INTO `data_matkul` (`id`, `kode`, `nama`, `jumlah_sks`, `semester_id`, `dosen_id`, `jenis_matkul`, `created_at`, `updated_at`) VALUES
-(7, 'MK002', 'Fisika', 2, 1, 2, 'Wajib', NULL, NULL),
-(8, 'MK003', 'Kimia', 2, 1, 2, 'Pilihan', NULL, NULL),
-(9, 'MK004', 'Biologi', 3, 1, 2, 'Pilihan', NULL, NULL),
-(10, 'MK005', 'Bahasa Inggris', 2, 1, 2, 'Wajib', NULL, NULL),
-(11, 'MK001', 'Android', 4, 1, 2, 'Wajib', '2024-03-24 11:38:56', '2024-03-24 11:38:56');
+(13, 'SVPL001', 'Ekonomi', 2, 4, 1, 'Pilihan', '2024-05-30 21:01:44', '2024-05-30 21:01:44');
 
 -- --------------------------------------------------------
 
@@ -166,7 +153,20 @@ CREATE TABLE `data_semester` (
 --
 
 INSERT INTO `data_semester` (`id`, `semester`) VALUES
-(1, 1);
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11, 11),
+(12, 12),
+(13, 13),
+(14, 14);
 
 -- --------------------------------------------------------
 
@@ -184,7 +184,8 @@ CREATE TABLE `departemen` (
 --
 
 INSERT INTO `departemen` (`id`, `nama_departemen`) VALUES
-(1, 'Teknik Informatika');
+(1, 'Teknik Informatika'),
+(2, 'Departmen teknik elektro dan informatika');
 
 -- --------------------------------------------------------
 
@@ -218,7 +219,10 @@ CREATE TABLE `fakultas` (
 --
 
 INSERT INTO `fakultas` (`id`, `nama`) VALUES
-(1, 'Fakultas Teknik');
+(1, 'Teknik'),
+(2, 'Vokasi'),
+(3, 'Mipa'),
+(4, 'FIB');
 
 -- --------------------------------------------------------
 
@@ -278,7 +282,9 @@ CREATE TABLE `jenjang_pendidikan` (
 --
 
 INSERT INTO `jenjang_pendidikan` (`id`, `nama`) VALUES
-(1, 'S1');
+(1, 'S1'),
+(2, 'D-IV'),
+(3, 'S2');
 
 -- --------------------------------------------------------
 
@@ -351,12 +357,14 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
-(13, 'App\\Models\\User', 1, 'user login', '49f31a63d6d854066ffe56cd48502822c6bee7bdeb5976497035eaddbe6a17ef', '[\"*\"]', NULL, NULL, '2024-03-24 00:25:10', '2024-03-24 00:25:10'),
-(14, 'App\\Models\\User', 1, 'user login', '5904f12aeea2ebff92e7aedf9d856dccc57f440abbc62e9a737f7d2298342f80', '[\"*\"]', NULL, NULL, '2024-03-24 04:29:29', '2024-03-24 04:29:29'),
 (15, 'App\\Models\\User', 1, 'user login', '7bc6d13422a79e5a5aa46cc584230edf73bdb544c5c20c0e142e442485b70d7c', '[\"*\"]', NULL, NULL, '2024-03-24 04:55:34', '2024-03-24 04:55:34'),
 (22, 'App\\Models\\User', 1, 'user login', '314fab495e5f5a98a25924d7fd44d963451858f03e1a29287e9ce64e5860ed6e', '[\"*\"]', NULL, NULL, '2024-03-24 09:54:57', '2024-03-24 09:54:57'),
 (26, 'App\\Models\\User', 1, 'user login', '0d7e266593933ed015b9bb25870413584adb283e2fa23a40d0d66a4d6e8eaeec', '[\"*\"]', '2024-03-24 11:01:56', NULL, '2024-03-24 10:53:02', '2024-03-24 11:01:56'),
-(27, 'App\\Models\\User', 1, 'user login', '907d5ee3506897a240d3c936781a7d01f088d2bd14f33dace1453f6a39861a41', '[\"*\"]', '2024-03-24 11:42:32', NULL, '2024-03-24 11:32:58', '2024-03-24 11:42:32');
+(27, 'App\\Models\\User', 1, 'user login', '907d5ee3506897a240d3c936781a7d01f088d2bd14f33dace1453f6a39861a41', '[\"*\"]', '2024-05-09 09:32:32', NULL, '2024-03-24 11:32:58', '2024-05-09 09:32:32'),
+(29, 'App\\Models\\User', 1, 'user login', '937cb10337579f469b0e9425f533aad272bfbbd565d891b307790dfe26ad17c5', '[\"*\"]', NULL, NULL, '2024-05-02 18:41:05', '2024-05-02 18:41:05'),
+(33, 'App\\Models\\User', 1, 'user login', 'c5eda36e748640d1fad84cef4d4e3774b22831894c57fc8a06e81cd90133788d', '[\"*\"]', '2024-05-03 02:20:50', NULL, '2024-05-02 23:28:38', '2024-05-03 02:20:50'),
+(39, 'App\\Models\\User', 1, 'user login', '80b2edf493a2800033052c8963f8cff98b0a93cb969ba1a060908082d669bbc5', '[\"*\"]', NULL, NULL, '2024-05-08 17:08:40', '2024-05-08 17:08:40'),
+(42, 'App\\Models\\User', 1, 'user login', '262ae2fa7eea98c6cc2a17cf4cde5f793a3068087cd79739fd4c0b4e70bc26eb', '[\"*\"]', '2024-05-31 00:11:01', NULL, '2024-05-08 17:15:24', '2024-05-31 00:11:01');
 
 -- --------------------------------------------------------
 
@@ -376,7 +384,11 @@ CREATE TABLE `prodi` (
 --
 
 INSERT INTO `prodi` (`id`, `nama_jurusan`, `fakultas_id`, `departemen_id`) VALUES
-(1, 'Teknik Informatika', 1, 1);
+(1, 'Teknik Informatika', 1, 1),
+(2, 'Teknologi rekayasa perangkat lunak', 2, 2),
+(3, 'Teknologi rekayasa internet', 2, 2),
+(4, 'Teknologi rekayasa elektro', 2, 2),
+(5, 'Teknologi rekayasa instrumentasi dan kontrol', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -399,7 +411,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `role`, `created_at`, `updated_at`, `remember_token`) VALUES
-(1, 'GnqG8PoQEF@gmail.com', '$2y$12$Sz5RQBHBxKVzu3o.7SqBiuYB9p59g3sDUl.ZwViIFw6.LxeUnQ4oa', 'admin', NULL, '2024-03-24 11:32:58', '$2y$12$ErxmM9wOVZnwD06HFF8XveEzzRf4fBkHFpPOpV1FS4SuKSyj2wovq');
+(1, 'GnqG8PoQEF@gmail.com', '$2y$12$Sz5RQBHBxKVzu3o.7SqBiuYB9p59g3sDUl.ZwViIFw6.LxeUnQ4oa', 'admin', NULL, '2024-05-08 17:15:24', '$2y$12$Gi8xNClVL0i.RCYGk0ZNCu53UF5N7wmGkeugzjfv9vSss6NTe0hJi');
 
 --
 -- Indexes for dumped tables
@@ -436,8 +448,8 @@ ALTER TABLE `data_mahasiswa`
 --
 ALTER TABLE `data_matkul`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `data_matkul_semester_id_foreign` (`semester_id`),
-  ADD KEY `data_matkul_dosen_id_foreign` (`dosen_id`);
+  ADD KEY `data_matkul_dosen_id_foreign` (`dosen_id`),
+  ADD KEY `data_matkul_semseter_id_foreign` (`semester_id`);
 
 --
 -- Indexes for table `data_semester`
@@ -527,7 +539,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `data_dosen`
 --
 ALTER TABLE `data_dosen`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `data_kelas`
@@ -539,25 +551,25 @@ ALTER TABLE `data_kelas`
 -- AUTO_INCREMENT for table `data_mahasiswa`
 --
 ALTER TABLE `data_mahasiswa`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `data_matkul`
 --
 ALTER TABLE `data_matkul`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `data_semester`
 --
 ALTER TABLE `data_semester`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `departemen`
 --
 ALTER TABLE `departemen`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -569,7 +581,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `fakultas`
 --
 ALTER TABLE `fakultas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `history_pendidikan`
@@ -587,7 +599,7 @@ ALTER TABLE `jabatan_dosen`
 -- AUTO_INCREMENT for table `jenjang_pendidikan`
 --
 ALTER TABLE `jenjang_pendidikan`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -599,13 +611,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -640,7 +652,7 @@ ALTER TABLE `data_mahasiswa`
 --
 ALTER TABLE `data_matkul`
   ADD CONSTRAINT `data_matkul_dosen_id_foreign` FOREIGN KEY (`dosen_id`) REFERENCES `data_dosen` (`id`),
-  ADD CONSTRAINT `data_matkul_semester_id_foreign` FOREIGN KEY (`semester_id`) REFERENCES `data_semester` (`id`);
+  ADD CONSTRAINT `data_matkul_semseter_id_foreign` FOREIGN KEY (`semester_id`) REFERENCES `data_semester` (`id`);
 
 --
 -- Constraints for table `history_pendidikan`
